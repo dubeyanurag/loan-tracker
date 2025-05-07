fdb5366c2f0ea5960c0548aca26545d7478208bc
Anurag Dubey <anurag@Anurags-MacBook-Air.local>
2025-05-07 21:17:39 +0530

    chore: Temporarily comment out base path for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/home-loan-tracker/', // Set base path for GitHub Pages
+  // base: '/home-loan-tracker/', // Set base path for GitHub Pages - Commented out for local dev
 })
